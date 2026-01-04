ba06d265ce87f1d5e7b240b9496565034c112e38
Manyam Jagadeeswar Reddy <manyamjagadeeswar7989@gmail.com>
2026-01-04 18:56:17 +0530

    feat(realtime): add WebSocket messaging and typing indicators

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import app from "./app";
 import connectDB from "./config/dbConnect";
 import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+import { setupSocket } from "./socket/socket";
 
 dotenv.config();
 
@@ -10,14 +13,34 @@ const startServer = async () => {
     try {
         await connectDB();
 
-        const server = app.listen(PORT, () => {
+        // const server = app.listen(PORT, () => {
+        //     console.log(`Server running on port ${PORT}`);
+        // });
+
+        // server.on("error", (err: any) => {
+        //     console.error("Server failed to start:", err.message);
+        //     process.exit(1);
+        // });
+
+
+        //create HTTP server
+        const httpServer = http.createServer(app);
+
+        //create socket server
+        const io = new Server(httpServer, {
+            cors: {
+                origin: "*",
+            },
+        });
+
+        //Intialize socket
+        setupSocket(io);
+
+        //start server
+        httpServer.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
 
-        server.on("error", (err: any) => {
-            console.error("Server failed to start:", err.message);
-            process.exit(1);
-        });
     } catch (error) {
         console.error("Startup error:", error);
         process.exit(1);
